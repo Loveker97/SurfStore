c5b83c1982f3109761ffc179f89b4306f76700da
Loveker97 <44221941+Loveker97@users.noreply.github.com>
2019-04-24 20:11:26 -0700

    Update block_store.py

--- py/block_store.py
+++ py/block_store.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 ##############################################################################
-# Project 2 - SurfStore
-# Peter Panourgias
-# Feng Jiang
+
+
+# Hang Zhang
 # block_store.py
 ##############################################################################
 import argparse
